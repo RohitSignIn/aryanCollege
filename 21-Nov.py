dea9af4f960fa54ee50eed59aaa5c6327f2ac514
RohitSignIn <rohitsinghsignin@gmail.com>
2023-11-21 12:35:28 +0530

    Done 5 questions

---
+++ 21-Nov.py
@@ -0,0 +1,45 @@
+# 1. SUM of all Entered Numbers
+# sum = 0
+# for i in [2,3,4,1,3,4,5]:
+#     sum = sum + i
+
+# print(sum)
+
+# 2. Count of Even No & Count of Odd Numbers
+# evenCnt = 0
+# oddCnt = 0
+
+# for i in [2,3,4,1,3,4,5]:
+#     if(i % 2 == 0):
+#         evenCnt = evenCnt+1
+#     else: 
+#         oddCnt = oddCnt+1
+
+# print(evenCnt)
+# print(oddCnt)
+
+# 3. First Even No || First Odd No
+# for i in [1,3,4,1,3,4,5]:
+#     if(i%2 == 0):
+#         print(i)
+#         break
+
+# 8. Display First Even without skipping the rest
+# gotEven = False
+# for i in [1,3,4,1,3,4,5]:
+#     if(i%2 == 0):
+#         gotEven = True
+#     if(gotEven):
+#         print(i)
+
+
+# 4. Maximum Number
+# a = [1,2,3,4,5,6]
+# max = a[0]
+# b = len(a)
+
+# for i in range(1, b, 1):
+#     if(max < a[i]):
+#         max = a[i]
+
+# print(max)
